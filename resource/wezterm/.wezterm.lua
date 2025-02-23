fc049f0aba696a8f7af96b2e2682e3048e2299f2
Crono <crono@cronolab.net>
2025-02-23 00:47:16 -0500

    Update wezterm config for Jetbrains Mono

--- resource/wezterm/.wezterm.lua
+++ resource/wezterm/.wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "Gruvbox Dark (Gogh)"
 
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 10.0
 
 -- and finally, return the configuration to wezterm
